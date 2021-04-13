87334238f420f0d8b5269b19dff3f75fe388241b
donastienkaroumbata <45631878+donastien@users.noreply.github.com>
2021-04-13 10:37:34 +0200

    init

---
+++ app.js
@@ -0,0 +1,13 @@
+const h1 = document.querySelector('.text');
+/*----------------------------------------------*/
+
+const tab = ['Thomas', 'Alex', 'Corentin'];
+for (let i in tab) {
+    console.log(tab[i]);
+}
+
+
+/*----------------------------------------------*/
+h1.innerHTML = sortie;
+
+
